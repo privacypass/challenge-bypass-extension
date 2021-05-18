55145cc427fde0ea7451b7478c7304c646eeea71
Pop Chunhapanya <pop@cloudflare.com>
2021-05-19 04:33:42 +0800

    Initial commit

---
+++ src/popup/components/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>hello, world!</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        popup:'./src/popup/index.tsx',
+        background: './src/background/index.ts',
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/icons', to: 'icons' },
+                { from: 'public/manifest.json' },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['popup'],
+            filename: 'popup.html',
+            template: 'public/popup.html',
+        }),
+    ],
+};
