1275bc3d5652a7bb0a225ecc18ab2e2bf0a4008f
Pop Chunhapanya <pop@cloudflare.com>
2021-05-20 15:19:58 +0800

    Body tag must not have a margin

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@popup/components/App';
 
+import '@popup/styles/body.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
