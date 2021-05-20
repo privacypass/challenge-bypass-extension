b4859d6d4771c69f008a8df274346e17d63db178
Pop Chunhapanya <pop@cloudflare.com>
2021-05-20 16:57:43 +0800

    Implement Button component

--- src/popup/components/App/index.tsx
+++ src/popup/components/App/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import Header from '@popup/components/Header';
-import Container from '@popup/components/Container';
-import PassButton from '@popup/components/PassButton';
 import styles from './styles.module.scss';
+
+import Header     from '@popup/components/Header';
+import Button     from '@popup/components/Button';
+import Container  from '@popup/components/Container';
+import PassButton from '@popup/components/PassButton';
 
 function App() {
     return (
@@ -11,6 +13,9 @@ function App() {
             <Container>
                 <PassButton value={0}>Cloudflare</PassButton>
                 <PassButton value={0}>hCaptcha</PassButton>
+                <Button>Log CF Redemption to Console</Button>
+                <Button>Clear All Passes</Button>
+                <Button>View on Github</Button>
             </Container>
         </div>
     );
