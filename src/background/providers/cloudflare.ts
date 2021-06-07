80e91eb75d3064712579969227d0aa59a6767221
Pop Chunhapanya <pop@cloudflare.com>
2021-06-07 16:31:51 +0800

    Change the context of the tab upon the response
    The HTTP header cf-chl-bypass is used to determine what Privacy Pass
    provider (Cloudflare or hCaptcha) the website supports. We change the
    tab context according to that header value.

---
+++ src/background/providers/cloudflare.ts
@@ -0,0 +1,6 @@
+export default class Cloudflare {
+    static readonly id: number = 1;
+
+    handleBeforeRequest(details: chrome.webRequest.WebRequestBodyDetails) {
+    }
+}
---
+++ src/background/providers/hcaptcha.ts
@@ -0,0 +1,6 @@
+export default class Hcaptcha {
+    static readonly id: number = 2;
+
+    handleBeforeRequest(details: chrome.webRequest.WebRequestBodyDetails) {
+    }
+}
