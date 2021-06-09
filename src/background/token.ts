25510048c35a9197204e2f46c2b4cdf5916e74ef
Pop Chunhapanya <pop@cloudflare.com>
2021-06-09 14:12:30 +0800

    Send Cloudflare issuance requests when appropriate

---
+++ src/background/token.ts
@@ -0,0 +1,24 @@
+import crypto from '@background/crypto';
+
+export default class Token {
+    private input:  crypto.Bytes;
+    private factor: crypto.BigNum;
+
+    private blindedPoint:   crypto.Point;
+    private unblindedPoint: crypto.Point;
+
+    constructor() {
+        const { data:  input,  point: unblindedPoint } = crypto.newRandomPoint();
+        const { blind: factor, point: blindedPoint   } = crypto.blindPoint(unblindedPoint);
+
+        this.input  = input;
+        this.factor = factor;
+
+        this.blindedPoint   = blindedPoint;
+        this.unblindedPoint = unblindedPoint;
+    }
+
+    getEncodedBlindedPoint(): string {
+        return crypto.sec1EncodeToBase64(this.blindedPoint, true); // true is for compression
+    }
+}
