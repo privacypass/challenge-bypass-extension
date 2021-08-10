cd86d8f590f776b5f7530a6ec0bf8156f995f7d0
Pop Chunhapanya <pop@cloudflare.com>
2021-08-10 16:04:49 +0800

    Add TODO comments

--- src/background/crypto.d.ts
+++ src/background/crypto.d.ts
@@ -9,15 +9,20 @@ declare namespace crypto {
     export type Hash       = 'sha256';
     export type HashMethod = 'increment' | 'swu';
 
+    // TODO This should be implemented in a new Point class.
     export function blindPoint(point: Point): { blind: BigNum, point: Point };
+    // TODO This should be implemented in a new Point class.
     export function newRandomPoint():         { data:  Bytes,  point: Point };
 
     export function getActiveECSettings(): any;
     export function initECSettings(params: { curve: Curve, hash: Hash, method: HashMethod });
 
+    // TODO This should be implemented in a new Point class.
     export function getCurvePoints(signatures: string[]): { points: Point[], compressed: boolean };
 
+    // TODO This should be implemented in a new Point class.
     export function sec1EncodeToBase64(point: Point, compressed: boolean): string;
+    // TODO This should be implemented in a new Point class.
     export function sec1DecodeFromBase64(encoded: string): Point;
 
     export function verifyConfiguration(publicKey: string, config: object, signature: string): boolean;
