881e3e7c0b001b5ce290ca5fbc445a563523f454
Pop Chunhapanya <pop@cloudflare.com>
2021-09-28 03:07:46 +0800

    Implement the provider button logic

---
+++ src/popup/components/HcaptchaButton/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+import { PassButton } from '@popup/components/PassButton';
+
+export function HcaptchaButton(): JSX.Element {
+    const openHomePage = () => {
+        chrome.tabs.create({ url: 'https://www.hcaptcha.com/privacy-pass' });
+    };
+
+    return <PassButton value={0} onClick={openHomePage}>Cloudflare</PassButton>;
+}
