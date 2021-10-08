af5af27a8fe8c989c483731b9a7524fceca1b1cc
Pop Chunhapanya <pop@cloudflare.com>
2021-10-08 20:18:05 +0800

    Implement the Github link button

--- src/popup/components/App/index.tsx
+++ src/popup/components/App/index.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@popup/components/Button';
 import { Container } from '@popup/components/Container';
 import { ClearButton } from '@popup/components/ClearButton';
 import { CloudflareButton } from '@popup/components/CloudflareButton';
 import { HcaptchaButton } from '@popup/components/HcaptchaButton';
+import { GithubButton } from '@popup/components/GithubButton';
 import { Header } from '@popup/components/Header';
 import React from 'react';
 import styles from './styles.module.scss';
@@ -14,9 +14,8 @@ export function App(): JSX.Element {
             <Container>
                 <CloudflareButton />
                 <HcaptchaButton />
-                <Button>Log CF Redemption to Console</Button>
                 <ClearButton />
-                <Button>View on Github</Button>
+                <GithubButton />
             </Container>
         </div>
     );
