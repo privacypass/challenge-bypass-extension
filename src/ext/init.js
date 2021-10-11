c83ff3989881adac572c403b69660000eedb9ec4
Pop Chunhapanya <pop@cloudflare.com>
2021-10-12 02:43:49 +0800

    Remove everything

--- src/ext/init.js
+++
@@ -1,9 +0,0 @@
-/**
- * Any initialisation processes that are required, but which should not
- * run during the test processes
- */
-
-// process any patches to the base configurations
-validConfigIds().forEach((id) => {
-    processConfigPatches(id);
-});
