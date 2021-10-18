5b312fd8a356687f0f0637f481ea562ae3751368
Pop Chunhapanya <pop@cloudflare.com>
2021-10-18 17:24:58 +0800

    Create Storage class
    We need a Storage class so that we can add middlewares for all the
    operations of the storage.

---
+++ src/background/storage.ts
@@ -0,0 +1,15 @@
+export class Storage {
+    private prefix: string;
+
+    constructor(prefix: string) {
+        this.prefix = prefix;
+    }
+
+    getItem(key: string): string | null {
+        return window.localStorage.getItem(`${this.prefix}-${key}`)
+    }
+
+    setItem(key: string, value: string): void {
+        window.localStorage.setItem(`${this.prefix}-${key}`, value)
+    }
+}
