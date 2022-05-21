79ee205db5355471f801e669a5ddaf31ca443b1a
armfazh <armfazh@cloudflare.com>
2022-05-20 23:27:09 -0700

    Tidy up blindrsa module.

--- src/blindrsa/util.test.ts
+++ src/blindrsa/util.test.ts
@@ -1,4 +1,4 @@
-import { emsa_pss_encode } from './util.js';
+import { emsa_pss_encode } from './util';
 import { jest } from '@jest/globals';
 // Test vector in file pss_test.go from: https://cs.opensource.google/go/go/+/refs/tags/go1.18.2:src/crypto/rsa/pss_test.go
 // Test vector in file pss-int.txt from: ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-1/pkcs-1v2-1-vec.zip
